f9faeaaad6772cce4630bc4fa2ea509491c5312e
lucastx <lucas@lucasteixeira.com>
2011-11-23 11:08:35 -0200

    consertei bug de remoção de despesas/ajustes

--- caixa/urls.py
+++ caixa/urls.py
@@ -19,10 +19,10 @@ urlpatterns = patterns('vestat.caixa.views',
      (r'^(\d{4})/(\d{2})/(\d{2})/venda/(?P<venda_id>\d+)/cartao/(?P<cartao_id>\d+)/remover$', 'remover_cartao'),
 
      (r'^(?P<ano>\d{4})/(?P<mes>\d{2})/(?P<dia>\d{2})/despesa/adicionar$', 'adicionar_despesa'),
-     (r'^(\d{4})/(\d{2})/(\d{2})/(?P<dia>\d{2})/despesa/(?P<id>\d+)/remover$', 'remover_despesa'),
+     (r'^(\d{4})/(\d{2})/(\d{2})/despesa/(?P<id>\d+)/remover$', 'remover_despesa'),
      
      (r'^(?P<ano>\d{4})/(?P<mes>\d{2})/(?P<dia>\d{2})/ajuste/adicionar$', 'adicionar_ajuste'),
-     (r'^(\d{4})/(\d{2})/(\d{2})/(?P<dia>\d{2})/ajuste/(?P<id>\d+)/remover$', 'remover_ajuste'),
+     (r'^(\d{4})/(\d{2})/(\d{2})/ajuste/(?P<id>\d+)/remover$', 'remover_ajuste'),
 
      (r'^(?P<ano>\d{4})/(?P<mes>\d{2})/(?P<dia>\d{2})/movbancaria/adicionar$', 'adicionar_movbancaria'),
      (r'^(\d{4})/(\d{2})/(\d{2})/movbancaria/(?P<id>\d+)/remover$', 'remover_movbancaria'),
