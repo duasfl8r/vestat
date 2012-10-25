07e5eca2879d085e8dac2f659f0e8250a9cdc561
lucastx <lucas@lucasteixeira.com>
2012-10-25 12:28:57 -0200

    remove linhas em branco dos relatórios CSV
    cria uma nova templatetag, 'stripempty', e a usa no template do
    relatório CSV

---
+++ relatorios/templatetags/relatorio_extras.py
@@ -0,0 +1,19 @@
+import re
+from django import template
+
+register = template.Library()
+
+@register.tag(name="stripempty")
+def do_stripempty(parser, token):
+    nodelist = parser.parse(('endstripempty',))
+    parser.delete_first_token()
+    return StripEmptyNode(nodelist)
+
+class StripEmptyNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+    def render(self, context):
+        output = self.nodelist.render(context)
+        lines = output.split("\n")
+        nonempty_lines = [l for l in lines if not re.match('^\w*$', l)]
+        return '\n'.join(nonempty_lines)
