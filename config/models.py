e9ae338b88960be0d3d1688584eab6cfed36c7d1
lucastx <lucas@lucasteixeira.com>
2012-11-06 12:58:00 -0200

    configuração de parcelas pra casa/funcionários

--- config/models.py
+++ config/models.py
@@ -1,7 +1,10 @@
+# -*- encoding: utf-8 -*-
 """
 Modelos de configuração do Vestat.
 
 """
+
+from fractions import Fraction
 
 from django.db import models
 
@@ -24,3 +27,35 @@ class VestatConfiguration(models.Model):
     """
     Saldo inicial dos 10%
     """
+
+    parcelas_10p_casa = models.IntegerField("Parcelas pra casa", default=1)
+    """
+    Parcelas dos 10% destinadas ao restaurante
+    """
+
+    parcelas_10p_funcionarios = models.IntegerField("Parcelas pros funcionários", default=3)
+    """
+    Parcelas dos 10% destinadas aos funcionários
+    """
+
+    @property
+    def _total_de_fatores_10p(self):
+        return self.parcelas_10p_casa + self.parcelas_10p_funcionarios
+
+    @property
+    def fracao_10p_casa(self):
+        """
+        Retorna a razão entre as parcelas pra casa e o total de
+        parcelas, como um objeto `fractions.Fraction`.
+
+        """
+        return Fraction(self.parcelas_10p_casa, self._total_de_fatores_10p)
+
+    @property
+    def fracao_10p_funcionarios(self):
+        """
+        Retorna a razão entre as parcelas pros funcionários e o total de
+        parcelas, como um objeto `fractions.Fraction`.
+
+        """
+        return Fraction(self.parcelas_10p_funcionarios, self._total_de_fatores_10p)
