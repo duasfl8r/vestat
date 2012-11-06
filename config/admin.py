db0441a4b2b85900647b4fa11f9a89483e8d5007
lucastx <lucas@lucasteixeira.com>
2012-11-06 12:58:40 -0200

    ModelAdmin pro VestatConfiguration

--- config/admin.py
+++ config/admin.py
@@ -1,1 +1,25 @@
-from vestat.caixa.models import VestatConfiguration
+# -*- encoding: utf-8 -*-
+
+from django.contrib import admin
+
+from vestat.config.models import VestatConfiguration
+
+class VestatConfigurationAdmin(admin.ModelAdmin):
+    fracao_10p_casa = lambda c: c.fracao_10p_casa
+    fracao_10p_casa.short_description = "Fração dos 10% pra casa"
+
+    fracao_10p_funcionarios = lambda c: c.fracao_10p_funcionarios
+    fracao_10p_funcionarios.short_description = "Fração dos 10% pros funcionários"
+
+    list_display = ["id", "saldo_inicial_gorjetas", fracao_10p_casa, fracao_10p_funcionarios]
+
+    fieldsets = [
+        ("10%",
+            {
+                "fields": ["saldo_inicial_gorjetas", "parcelas_10p_casa", "parcelas_10p_funcionarios"],
+            }
+        ),
+
+    ]
+
+admin.site.register(VestatConfiguration, VestatConfigurationAdmin)
