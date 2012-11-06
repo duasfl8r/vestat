53d594d0ffddf7c197644dac0b634ff010f55a87
lucastx <lucas@lucasteixeira.com>
2012-11-06 12:56:59 -0200

    documentação e classe Meta (django)

--- config/models.py
+++ config/models.py
@@ -1,5 +1,26 @@
-from decimal import Decimal
+"""
+Modelos de configuração do Vestat.
+
+"""
+
 from django.db import models
 
 class VestatConfiguration(models.Model):
-    saldo_inicial_gorjetas = models.DecimalField("Saldo inicial dos 10%", max_digits=10, decimal_places=2, default=0)
+    """
+    Configurações gerais do Vestat.
+
+    Esse modelo deve ser usado como um Singleton -- a única instância
+    dele é a definida pela variável `ID_CONFIG` do `settings`.
+    """
+
+    class Meta:
+            verbose_name = "Configuração do Vestat"
+            verbose_name_plural = "Configurações do Vestat"
+
+    # Gorjeta
+
+    saldo_inicial_gorjetas = models.DecimalField("Saldo inicial dos 10%",
+            max_digits=10, decimal_places=2, default=0)
+    """
+    Saldo inicial dos 10%
+    """
