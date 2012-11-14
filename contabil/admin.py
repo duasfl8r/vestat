f52ab46cc35dd7663c803f9653619cc098900526
lucastx <lucas@lucasteixeira.com>
2012-11-14 14:39:05 -0200

    deixa os forms do módulo contabil localized

--- contabil/admin.py
+++ contabil/admin.py
@@ -1,30 +1,43 @@
 # -*- encoding: utf-8 -*-
 
+import django.forms
 from django.db import models
-from django.forms.widgets import TextInput
 from django.contrib import admin
 
 from models import Registro, Transacao, Lancamento
+
+from vestat.utils import LocalizedModelForm
 
 class RegistroAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Registro, RegistroAdmin)
 
+class LancamentoForm(LocalizedModelForm):
+    class Meta:
+        model = Lancamento
+
 class LancamentoInline(admin.TabularInline):
     model = Lancamento
+    form = LancamentoForm
     extra = 4
 
     formfield_overrides = {
         models.TextField: {
-            "widget": TextInput(attrs={"size": "40"}),
+            "widget": django.forms.widgets.TextInput(attrs={"size": "40"}),
         },
     }
+
+class TransacaoForm(LocalizedModelForm):
+    class Meta:
+        model = Transacao
+
 
 class TransacaoAdmin(admin.ModelAdmin):
     list_display = ["data", "descricao"]
     search_fields = ["descricao"]
     list_filter = ["data", "lancamentos__conta", "lancamentos__valor"]
+    form = TransacaoForm
 
     inlines = [
         LancamentoInline,
