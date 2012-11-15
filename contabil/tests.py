cfd0a2d749fbfed2b96fab52f034571746a35d6f
lucastx <lucas@lucasteixeira.com>
2012-11-15 19:51:02 -0200

    testes pro contabil.Contas

--- contabil/tests.py
+++ contabil/tests.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 
-from contabil import join
+from contabil import join, Contas
 from contabil.models import Registro, Transacao, Lancamento
 
 class RegistroTestCase(TestCase):
@@ -96,4 +96,83 @@ class LancamentoTestCase(TestCase):
     def test_lancamento_sem_conta_dah_erro(self):
         lancamento = Lancamento(transacao=self.transacao, valor=Decimal("10.00"))
         self.assertRaises(ValidationError, lambda: lancamento.save())
+
+
+class ContasTestCase(TestCase):
+    def setUp(self):
+        self.registro = Registro(nome="Registro teste")
+        self.registro.save()
+
+        self.transacao = Transacao(
+                registro=self.registro,
+                data=datetime.date.today(),
+                descricao="Transação de teste"
+        )
+        self.transacao.save()
+
+    def teste_com_uma_conta(self):
+        self.transacao.lancamentos.create(
+                valor=Decimal("10.00"),
+                conta=join("gastos", "funcionarios"),
+        )
+
+        self.transacao.lancamentos.create(
+                valor=Decimal("10.00"),
+                conta=join("gastos", "fornecedores", "açougue"),
+        )
+
+        self.transacao.lancamentos.create(
+                valor=Decimal("10.00"),
+                conta=join("gastos", "fornecedores", "mercado"),
+        )
+
+        self.transacao.lancamentos.create(
+                valor=Decimal("10.00"),
+                conta=join("gastos", "fornecedores", "vinho"),
+        )
+
+        self.transacao.lancamentos.create(
+                valor=Decimal("10.00"),
+                conta=join("gastos", "impostos"),
+        )
+
+        self.transacao.lancamentos.create(
+                valor=Decimal("-50.00"),
+                conta=join("bens", "caixa"),
+        )
+
+        hierarquia_do_registro = Contas()
+        hierarquia_do_registro.digerir_registro(self.registro)
+
+
+        hierarquia_das_transacoes = Contas()
+        hierarquia_das_transacoes.digerir_transacoes([self.transacao])
+
+
+        hierarquia_dos_lancamentos = Contas()
+        hierarquia_dos_lancamentos.digerir_lancamentos(self.transacao.lancamentos.all())
+
+        self.assertEqual(hierarquia_do_registro, hierarquia_das_transacoes)
+        self.assertEqual(hierarquia_das_transacoes, hierarquia_dos_lancamentos)
+
+        resultado_correto = {
+            u"gastos": {
+                u"fornecedores": {
+                    u"mercado": {},
+                    u"açougue": {},
+                    u"vinho": {},
+                },
+
+                u"impostos": {},
+
+                u"funcionarios": {},
+
+            },
+
+            u"bens": {
+                u"caixa": {},
+            },
+        }
+
+        self.assertEqual(hierarquia_dos_lancamentos.contas, resultado_correto)
 
