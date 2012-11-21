9dceceb1900717aea5cb25d719926052a644cb15
lucastx <lucas@lucasteixeira.com>
2012-11-21 16:34:47 -0200

    remove arquivos obsoletos da app config
    esses arquivos eram usados na tela de configuração anterior, mas foram substituídos pelo Django Admin

--- config/forms.py
+++
@@ -1,6 +0,0 @@
-import django.forms
-from models import *
-
-class VestatConfigurationForm(django.forms.ModelForm):
-    class Meta:
-        model = VestatConfiguration
--- config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from views import *
-
-urlpatterns = patterns('',
-     (r'^$', index),
-     (r'^resetar_bd/$', resetar_bd),
-     (r'^exportar/$', exportar),
-)
