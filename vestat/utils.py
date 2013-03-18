eff286c4cd5fa44ac32ed4474b527b97a438a7c5
lucastx <lucas@lucasteixeira.com>
2013-03-18 20:02:52 -0300

    mkdir_p

--- vestat/utils.py
+++ vestat/utils.py
@@ -1,1 +1,18 @@
 # -*- encoding: utf-8 -*-
+import os, errno
+
+"""
+Classes e funções utilitárias que NÃO dependem do Django.
+"""
+
+def mkdir_p(path):
+    """
+    Cria o diretório fornecido (e seus pais, se necessário) caso ele não exista.  (imita `mkdir -p`)
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
