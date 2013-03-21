5889a70dc41a9a74e39a5d2bab8ba5e5870a8b6c
lucastx <lucas@lucasteixeira.com>
2013-03-21 17:23:06 -0300

    comando 'atualizar 1.2.1' converte fixtures pra novos modelos

--- vestat/config/management/commands/atualizar.py
+++ vestat/config/management/commands/atualizar.py
@@ -1,10 +1,14 @@
 # -*- encoding: utf-8 -*-
+import os
+
 from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 
-def versao_1_2_0():
+from vestat.config.management.converter_dump import converter_cartoes
+
+def versao_1_2_0(cmd, *args):
     # Sincroniza o banco de dados
     call_command('syncdb', interactive=False)
 
@@ -19,6 +23,27 @@ def versao_1_2_0():
     superuser.set_password(settings.AUTOLOGIN_PASSWORD)
     superuser.save()
 
+def versao_1_2_1(cmd, *args):
+    if len(args) != 1:
+        raise CommandError("Uso: atualizar 1.2.1 <dump_file>\n\ndump: arquivo de dump do banco de dados anterior (JSON)")
+
+
+    print("Convertendo cartões de crédito e pagamentos para novo modelo...")
+
+    novo_bd_json = converter_cartoes(args[0])
+    tmp_dump_filename = "fixture_convertida.json"
+
+    print("Armazenando dados convertidos em {tmp_dump_filename}".format(**vars()))
+
+    with open(tmp_dump_filename, "w") as tmp_dump_file:
+        tmp_dump_file.write(novo_bd_json)
+
+    print("Carregando dados convertidos no banco de dados...")
+
+    call_command("loaddata", tmp_dump_filename)
+
+    print("Removendo arquivo temporário...")
+    os.remove(tmp_dump_filename)
 
 
 class Command(BaseCommand):
@@ -29,8 +54,8 @@ class Command(BaseCommand):
         versoes_disponiveis = { nome.replace("versao_", "").replace("_", "."): item for nome, item in globals().items() if nome.startswith("versao_") }
         nomes_das_versoes = [ nome for nome, _ in versoes_disponiveis.items() ]
 
-        if len(args) != 1:
-            raise CommandError("Uso: atualizar " + self.args + "\n" + \
+        if len(args) < 1:
+            raise CommandError("Uso: atualizar " + self.args + " [ARG...]\n" + \
                 "\nVersões disponíveis:\n\n" + \
                 "\n".join(nomes_das_versoes))
 
@@ -42,4 +67,4 @@ class Command(BaseCommand):
             raise CommandError("\nVersões disponíveis:\n\n" + \
                 "\n".join(nomes_das_versoes))
 
-        funcao()
+        funcao(self, *args[1:])
---
+++ vestat/config/management/converter_dump.py
@@ -0,0 +1,91 @@
+# -*- encoding: utf-8 -*-
+import codecs
+import json
+
+last_bandeira_pk = 0
+
+def converter_cartoes(filename):
+    bandeira_antiga_pra_nova = {}
+
+    def handle_bandeira(bandeira):
+        global last_bandeira_pk
+
+        bandeira_nova_credito = {
+            u"pk": last_bandeira_pk + 1,
+            u"model": u"caixa.bandeira",
+
+            u"fields": {
+                u"nome": bandeira["fields"]["nome"] + u" Crédito",
+                u"taxa": bandeira["fields"]["taxa_credito"],
+                u"prazo_de_deposito": 31,
+                u"contagem_de_dias": "C",
+                u"categoria": "C",
+            }
+        }
+
+        bandeira_nova_debito = {
+            u"pk": last_bandeira_pk + 2,
+            u"model": u"caixa.bandeira",
+
+            u"fields": {
+                u"nome": bandeira["fields"]["nome"] + u" Débito",
+                u"taxa": bandeira["fields"]["taxa_debito"],
+                u"prazo_de_deposito": 2,
+                u"contagem_de_dias": "U",
+                u"categoria": "C",
+            },
+        }
+
+        last_bandeira_pk += 2
+
+        bandeira_antiga_pra_nova[bandeira["pk"]] = {
+            "C": bandeira_nova_credito["pk"],
+            "D": bandeira_nova_debito["pk"],
+        }
+
+        return bandeira_nova_credito, bandeira_nova_debito
+
+    def handle_pagamentocomcartao(pagamentocomcartao):
+        bandeira_antiga = pagamentocomcartao["fields"]["bandeira"]
+        categoria = pagamentocomcartao["fields"]["categoria"]
+
+        bandeira_nova = bandeira_antiga_pra_nova[bandeira_antiga][categoria]
+
+        pagamentocomcartao_novo = {
+            u"pk": pagamentocomcartao["pk"],
+            u"model": u"caixa.pagamentocomcartao",
+
+            u"fields": {
+                u"venda": pagamentocomcartao["fields"]["venda"],
+                u"bandeira": bandeira_nova,
+                u"valor": pagamentocomcartao["fields"]["valor"],
+            },
+        }
+
+        return [pagamentocomcartao_novo]
+
+
+    print("Carregando arquivo de dump...")
+
+    dump = json.load(codecs.open(filename, encoding="utf-8"))
+
+    model_and_function = (
+        ("caixa.bandeira", handle_bandeira),
+        ("caixa.pagamentocomcartao", handle_pagamentocomcartao)
+    )
+
+    not_to_be_converted = [e for e in dump if e['model'] not in [m[0] for m in model_and_function]]
+
+    print("Convertendo modelos no formato antigo...")
+
+    result = []
+
+    for model, func in model_and_function:
+        for elem in [e for e in dump if e['model'] == model]:
+            result.extend(func(elem))
+
+    result.extend(not_to_be_converted)
+
+    print("Transformando resultado em JSON")
+
+    return json.dumps(result)
