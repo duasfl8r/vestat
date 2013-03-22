21f77d3d2463a59773e4c5aadf5d507b729d1ba6
lucastx <lucas@lucasteixeira.com>
2013-03-21 21:09:12 -0300

    melhora listagem de bandeiras no django admin

--- vestat/caixa/admin.py
+++ vestat/caixa/admin.py
@@ -15,7 +15,9 @@ class BandeiraAdmin(admin.ModelAdmin):
     Representação do modelo `Bandeira` no Django admin site.
     """
 
-    list_display = ["nome", "taxa", "prazo_de_deposito", "contagem_de_dias", "categoria"]
+    list_display = ["ativa", "nome", "taxa", "prazo_de_deposito", "contagem_de_dias", "categoria"]
+    list_display_links = ["ativa", "nome"]
+    ordering = ["-ativa", "nome"]
 
 admin.site.register(Bandeira, BandeiraAdmin)
 
