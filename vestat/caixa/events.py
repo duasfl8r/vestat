917f9c3e92a8cef0b28a52d90b6f4d33233ef34f
lucastx <lucas@lucasteixeira.com>
2013-04-03 14:21:43 -0300

    adiciona links pros dias do caixa como eventos do calendário

--- vestat/caixa/events.py
+++ vestat/caixa/events.py
@@ -1,11 +1,34 @@
 # -*- encoding: utf-8 -*-
 import logging
 
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
+
 from vestat.django_utils import format_currency, format_date
 from vestat.calendario import Event
 from models import PagamentoComCartao
+from views import ver_dia
 
 logger = logging.getLogger("vestat")
+
+def daterange_inclusive(d1, d2):
+    from datetime import timedelta
+    while d1 <= d2:
+        yield d1
+        d1 += timedelta(1)
+
+def dia_events(begin, end):
+    for d in daterange_inclusive(begin, end):
+        kwargs = {
+            "ano": "{0:04d}".format(d.year),
+            "mes": "{0:02d}".format(d.month),
+            "dia": "{0:02d}".format(d.day),
+        }
+
+        link = reverse(ver_dia, kwargs=kwargs)
+        nome = u'<a href="{link}">Caixa</a>'.format(link=link)
+
+        yield Event(d, mark_safe(nome))
 
 def dias_de_deposito_events(begin, end):
     pagamentos = PagamentoComCartao.objects.filter(data_do_deposito__range=(begin, end))
