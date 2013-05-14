403b1b3368949307096571f53c9aa0357a935158
lucastx <lucas@lucasteixeira.com>
2013-05-14 19:29:05 -0300

    substitui set por dict no armaz. de configurações
    usa o slug do link como chave do dicionário, impedindo que um link
    seja adicionado duas vezes, como acontecia.

--- vestat/config/core.py
+++ vestat/config/core.py
@@ -15,21 +15,21 @@ class Page():
     def __init__(self, name):
         self.name = name
         self._configurations = {}
-        self._default = set()
+        self._default = {}
 
     def add(self, link, group_name=None):
         if group_name:
-            group = self._configurations.setdefault(group_name, set())
+            group = self._configurations.setdefault(group_name, {})
         else:
             group = self._default
 
-        group.add(link)
+        group[link.slug] = link
 
     def groups(self):
         group_tuples = [("", self._default)] + self._configurations.items()
 
         return [
-            { "name": n, "links": l }
+            { "name": n, "links": l.values() }
             for n, l in group_tuples
         ]
 
